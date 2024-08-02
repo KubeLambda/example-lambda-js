b2a5a65fd82fe86d83e2dd9c5704c2c349e9aacf
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-08-02 20:56:47 +0300

    deps: update projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,19 @@
-const { javascript } = require("projen");
+const { TrailingComma } = require('projen/lib/javascript')
+const { javascript } = require('projen')
+
 const project = new javascript.NodeProject({
   authorName: "Bohdan Frankovskyi",
   defaultReleaseBranch: "main",
   name: "example-lambda-js",
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  description: 'KubeLambda JavaScript example lambda',
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 100,
+      semi: false,
+      singleQuote: true,
+      trailingComma: TrailingComma.ALL,
+    },
+  },
 });
 project.synth();
